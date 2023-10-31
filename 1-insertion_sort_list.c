ca116ab2102837c9b8959d37a2abcc3e4251ce4b
Hesham <dev@Heshamorabi.com>
2023-10-31 16:13:06 +0200

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *prev_node, *cur_node, *node_turn;
+
+	node_turn = *list;
+	while (node_turn)
+	{
+		if (node_turn->prev == NULL)
+		{
+			node_turn = node_turn->next;
+			continue;
+		}
+		cur_node = node_turn;
+		while (cur_node->n < cur_node->prev->n)
+		{
+			prev_node = cur_node->prev;
+
+			cur_node->prev = prev_node->prev;
+			prev_node->next = cur_node->next;
+			cur_node->next = prev_node;
+			prev_node->prev = cur_node;
+			if (prev_node->next)
+				prev_node->next->prev = prev_node;
+			if (cur_node->prev)
+				cur_node->prev->next = cur_node;
+			else
+			{
+				*list = cur_node;
+				print_list(*list);
+				break;
+			}
+			print_list(*list);
+		}
+		node_turn = node_turn->next;
+	}
+}
